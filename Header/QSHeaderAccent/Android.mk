70b05f7e3579f7d7590456c09c36f3959214a26e
Varun Date <date.varun123@gmail.com>
2020-09-07 02:03:22 +1400

    Build HeaderAccent as an overlay
    Signed-off-by: Varun Date <date.varun123@gmail.com>

--- Header/QSHeaderAccent/Android.mk
+++ Header/QSHeaderAccent/Android.mk
@@ -1,10 +1,13 @@
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
-CAL_MODULE_TAGS := optional
 
-LOCAL_PACKAGE_NAME := QSHeaderAccent
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_PACKAGE_NAME := QSHeaderAccentOverlay
+LOCAL_RRO_THEME := QSHeaderAccent
 LOCAL_SDK_VERSION := current
 LOCAL_CERTIFICATE := platform
+LOCAL_PRODUCT_MODULE := true
 LOCAL_PRIVILEGED_MODULE := false
 
-include $(BUILD_PACKAGE)
+include $(BUILD_RRO_PACKAGE)
